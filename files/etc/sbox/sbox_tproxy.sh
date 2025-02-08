045b3d5e64fba025e20143f23452478489b7d9cc
eze-root <labezeswu@outlook.com>
2025-02-08 21:02:56 +0800

    feat: add monitor.sh

--- files/etc/sbox/sbox_tproxy.sh
+++ files/etc/sbox/sbox_tproxy.sh
@@ -2,6 +2,7 @@
 # sing-box monitor
 
 SERVICE_NAME="sing-box"
+SBOX_DIR="/tmp/sing-box"
 SBOX_PATH="/tmp/sing-box/sing-box"
 SBOX_CONFIG_PATH="/tmp/sing-box/config.json"
 SBOX_CONFIG_PATH_NEW="/tmp/sing-box/config-new.json"
@@ -14,10 +15,13 @@ error_exit() {
     exit "${2:-1}"
 }
 
-# no SBOX
+mkdir -p $SBOX_DIR
+
 if [ ! -f "$SBOX_PATH" ]; then
     echo "$(date '+%Y-%m-%d %H:%M:%S') - ${SERVICE_NAME} not found"
 
+    sh /etc/sbox/sbox_tproxy_stop.sh
+    rm -rf /tmp/sing-box/*
     wget --no-check-certificate -O $SBOX_PATH $SBOX_URL 
     chmod +x $SBOX_PATH
 fi
@@ -27,15 +31,20 @@ if ! pgrep -f "${SERVICE_NAME}" > /dev/null; then
     wget --no-check-certificate -O ${SBOX_CONFIG_PATH_NEW} $SBOX_CONFIG_URL
     mv $SBOX_CONFIG_PATH_NEW $SBOX_CONFIG_PATH
     echo "$(date '+%Y-%m-%d %H:%M:%S') - ${SERVICE_NAME} to start" 
+    sh /etc/sbox/sbox_tproxy_stop.sh
     sh /etc/sbox/sbox_tproxy_start.sh
     /etc/init.d/${SERVICE_NAME} start
 else
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - ${SERVICE_NAME} Downloading config" 
-    wget --no-check-certificate -O ${SBOX_CONFIG_PATH_NEW} $SBOX_CONFIG_URL
-    mv $SBOX_CONFIG_PATH_NEW $SBOX_CONFIG_PATH
-    sh /etc/sbox/sbox_tproxy_stop.sh
-    sh /etc/sbox/sbox_tproxy_start.sh
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - ${SERVICE_NAME} to reload" 
-    /etc/init.d/${SERVICE_NAME} reload
+    current_hour=$(date +%H)
+    if [ "$current_hour" -ge 1 ] && [ "$current_hour" -lt 2 ]; then
+        echo "$(date '+%Y-%m-%d %H:%M:%S') - ${SERVICE_NAME} 1am and 2am, reload" 
+        sh /etc/sbox/sbox_tproxy_stop.sh
+        sh /etc/sbox/sbox_tproxy_start.sh
+        wget --no-check-certificate -O ${SBOX_CONFIG_PATH_NEW} $SBOX_CONFIG_URL
+        mv $SBOX_CONFIG_PATH_NEW $SBOX_CONFIG_PATH
+        echo "$(date '+%Y-%m-%d %H:%M:%S') - ${SERVICE_NAME} reload" 
+        /etc/init.d/${SERVICE_NAME} reload
+    else
+        echo "$(date '+%Y-%m-%d %H:%M:%S') - Skipping"
+    fi
 fi
-
